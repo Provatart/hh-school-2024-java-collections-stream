07919bacd93fe3b66cd6bfb843e13ba8ee03e784
Artiom <partobox@gmail.com>
2024-12-05 00:22:30 +0700

    Resolve Task 8

--- src/main/java/tasks/Task8.java
+++ src/main/java/tasks/Task8.java
@@ -4,8 +4,11 @@ import common.Person;
 import common.PersonService;
 import common.PersonWithResumes;
 import common.Resume;
+
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /*
   Еще один вариант задачи обогащения
@@ -21,7 +24,26 @@ public class Task8 {
   }
 
   public Set<PersonWithResumes> enrichPersonsWithResumes(Collection<Person> persons) {
-    Set<Resume> resumes = personService.findResumes(Set.of());
-    return Set.of();
+
+    Set<Resume> resumes = personService.findResumes(persons.stream().map(Person::id).collect(Collectors.toSet()));
+
+    Set<PersonWithResumes> personWithResumes = new HashSet<>();
+
+    Set<Resume> personsResumes = new HashSet<>();
+
+    for (Person person : persons) {
+
+      for (Resume resume : resumes) {
+        if (resume.personId() == person.id()) {
+          personsResumes.add(resume);
+        }
+      }
+
+      personWithResumes.add(new PersonWithResumes(person, new HashSet<>(personsResumes)));
+      personsResumes.clear();
+    }
+    return personWithResumes;
+
+
   }
 }
